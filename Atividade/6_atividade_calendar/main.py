bcf9259936abcefea209b1df2ffa124f4279ec2f
ale09carvalho <ale09.carvalho@gmail.com>
2025-06-18 23:25:17 -0300

    Aula  ativ 18/06 date

--- Atividade/6_atividade_calendar/main.py
+++ Atividade/6_atividade_calendar/main.py
@@ -7,7 +7,24 @@ import os
 import calendar as c
 
 try:
-    ...
-
+    while True:
+        ano = int(input("Informe o ano: "))
+        os.system("cls" if os.name == "nt" else "clear")
+        if ano >= 1900:
+            print(c.calendar(ano))
+            while True:
+                voltar = input("Deseja imprimir outro calendário? (s/n)").lower().strip()
+                if voltar == "s" or voltar == "n":
+                    break
+                else:
+                    print("Opção inválida.")
+                    continue
+            match voltar:
+                case "s":
+                    continue
+                case "n":
+                    break
+        else:
+            print("Calendário do ano não disponível.")
 except Exception as e:
     print (f"Não foi possível imprimir o calendário. {e}")
