bc1c6c288d8f3a13f67150798a7bce32e0429d34
ale09carvalho <ale09.carvalho@gmail.com>
2025-06-18 22:29:00 -0300

    resoluçao do desafio 18/6

--- Atividade/4_atividade_horAtual/main.py
+++ Atividade/4_atividade_horAtual/main.py
@@ -3,18 +3,23 @@
 # NOTE - para  interroper o programa, use a tecla de atalho Ctrl + C
 '''
 import datetime
-from datetime import date
+from datetime import datetime
 import time
+import os
 
-agora = datetime.datetime.now()
-hora_formatada = agora.strftime("%H:%M:%S")
+print("Pressuione Ctrl + C a qualquer momento para interroper o programa.")
+print("--------")
 
-while True:
-    try:
-        print("Hora atual: {hora_formatada}")
-        time.sleep(1)
-        break
-
-    except Exception as e:
-        print(f"Não foi possivel realizar a operaçao. {e}.")
-
+try:
+    while True:
+        # Limpar o terminal antes de imprimir a nova hora.
+        # 'cls' p/ Windows e 'clear' p/ sistemas Unix (Linux/macOs)
+        os.system("cls" if os.name == "nt" else "clear")
+        
+        horaAtual = datetime.now().strftime("%H:%M:%S")
+        print(f"Hora atual: {horaAtual}")
+        time.sleep(1) # Espera 1 segundo antes de atualizar novamente
+except KeyboardInterrupt:
+    print("\n Programa interropido pelo usuario")
+except Exception as e:
+    print(f"Ocorreu um erro : {e}")
--- Atividade/5_atividade_filme/main.py
+++ Atividade/5_atividade_filme/main.py
@@ -11,3 +11,54 @@ Usuario deve escolher a sala que esta passando o filme que deseja assistir.
 e deseja bom filme, e em seguida, encerra o programa.
 - Se o usuário não tiver a idade minima para ver o filme, o programa bloqueia a sua entrada, e re-exibe o menu de filmes para que ele escolha outro filme.
 '''
+import datetime
+from datetime import date
+
+hoje = date.today().strftime("%d/%m/%Y")
+hora = datetime.datetime.now().strftime("%H:%M:%S")
+
+try:
+    nome = input("Informe seu nome: ")
+    idade = int(input("Informe sua idade: "))
+
+    #laço repetição
+    while True:
+
+        print(f"{'-'*20}LISTA DE FILMES DISPONÍVEL{'-'*20}\n")
+        print ("Sala 1 - A Roda Quadrada - LIVRE")
+        print ("Sala 2 - A Volta Dos Que Não Foram - 12 anos")
+        print ("Sala 3 - A Trança Do Rei Careca - 14 anos")
+        print ("Sala 4 - Poeira em Alto Mar - 16 anos")
+        print ("Sala 5 - A Vingança do Peixe Frito - 18 anos")
+
+        # Opção de sala desejada
+        sala = input(f"\nSala escolhida: ")
+
+        # verifica a sala escolhida
+        match sala:
+            case "1":
+                idade_minima = 0
+            case "2":
+                idade_minima = 12
+            case "3":
+                idade_minima = 14
+            case "4":
+                idade_minima = 16
+            case "5":
+                idade_minima = 18
+            case _:
+                idade_minima = idade+1 # Obriga o usuario escolher uma sala valida
+        
+        # Verificar se o usuario tem a idade minima para assistir o filme
+
+        if idade >= idade_minima:
+            print(f"Olá, {nome} no dia: {hoje} as: {hora}, por favor dirija a sala {sala} escolhida. Tenha um bom filme!")
+            break # encerra laço
+        else:
+            print(f"{nome}, você não tem idade para assistir a esse filme.")
+            print("Por favor, escolha outra opçcao de filme.")
+            continue # continua o laço de repeticao
+except Exception as e:
+    print (f"Não foi possivel executar a operaçao! {e}.")
+finally:
+    print("Programa encerrado")
